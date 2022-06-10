09efefcfe64405b594aab09fa789d5fb729e6851
Jonny Browning <jonathan.browning@datatonic.com>
2022-06-10 14:57:58 +0100

    docs: updated example to just use main.tf

--- examples/hello_world_local/main.tf
+++ examples/hello_world_local/main.tf
@@ -1,15 +1,17 @@
 module "hello_world_pipeline" {
-  source                       = "../../"
-  project                      = var.project
-  vertex_region                = var.region
-  cloud_scheduler_region       = var.region
-  pipeline_spec_path           = var.pipeline_spec_path
-  parameter_values             = var.parameter_values
-  gcs_output_directory         = var.gcs_output_directory
-  vertex_service_account_email = var.vertex_service_account_email
-  time_zone                    = var.time_zone
-  schedule                     = var.schedule
-  cloud_scheduler_sa_email     = var.cloud_scheduler_sa_email
-  cloud_scheduler_job_name     = var.cloud_scheduler_job_name
+  source                 = "../../"
+  project                = "my-gcp-project-id"
+  vertex_region          = "europe-west2"
+  cloud_scheduler_region = "europe-west2"
+  pipeline_spec_path     = "pipeline.json"
+  parameter_values = {
+    "text" = "Hello, world!"
+  }
+  gcs_output_directory         = "gs://my-bucket/my-output-directory"
+  vertex_service_account_email = "my-vertex-service-account@my-gcp-project-id.iam.gserviceaccount.com"
+  time_zone                    = "UTC"
+  schedule                     = "0 0 * * *"
+  cloud_scheduler_sa_email     = "my-cloud-scheduler-service-account@my-gcp-project-id.iam.gserviceaccount.com"
+  cloud_scheduler_job_name     = "my-first-pipeline"
 
 }
