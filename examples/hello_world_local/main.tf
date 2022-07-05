20235a20042d24633342799760b3d82cfe01e535
Jonny Browning <jonathan.browning@datatonic.com>
2022-07-05 17:04:21 +0100

    test: remove most variables from example

--- examples/hello_world_local/main.tf
+++ examples/hello_world_local/main.tf
@@ -1,17 +1,16 @@
 module "hello_world_pipeline" {
   source                 = "../../"
   project                = var.project
-  vertex_region          = var.vertex_region
-  cloud_scheduler_region = var.cloud_scheduler_region
-  pipeline_spec_path     = var.pipeline_spec_path
+  vertex_region          = "europe-west2"
+  cloud_scheduler_region = "europe-west2"
+  pipeline_spec_path     = "pipeline.json"
   parameter_values = {
     "text" = "Hello, world!"
   }
-  gcs_output_directory         = var.gcs_output_directory
-  vertex_service_account_email = var.vertex_service_account_email
+  gcs_output_directory         = "gs://my-bucket/my-output-directory"
+  vertex_service_account_email = "my-vertex-service-account@my-gcp-project-id.iam.gserviceaccount.com"
   time_zone                    = "UTC"
   schedule                     = "0 0 * * *"
-  cloud_scheduler_sa_email     = var.cloud_scheduler_sa_email
-  cloud_scheduler_job_name     = var.cloud_scheduler_job_name
+  cloud_scheduler_job_name     = "pipeline-from-local-spec"
 
 }
