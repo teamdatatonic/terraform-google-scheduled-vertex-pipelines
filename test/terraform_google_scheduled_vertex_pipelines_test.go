4ba7d1a8b0ffee04be9eddde47ea54f6cbf848d6
Jonny Browning <jonathan.browning@datatonic.com>
2022-07-06 17:05:27 +0100

    ci: add GCS copy to CI pipeline instead of terratest

--- test/terraform_google_scheduled_vertex_pipelines_test.go
+++ test/terraform_google_scheduled_vertex_pipelines_test.go
@@ -2,12 +2,9 @@ package terraform_google_scheduled_vertex_pipelines_test
 
 import (
 	"context"
-	"io"
-	"os"
 	"testing"
 
 	scheduler "cloud.google.com/go/scheduler/apiv1beta1"
-	"cloud.google.com/go/storage"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 	schedulerpb "google.golang.org/genproto/googleapis/cloud/scheduler/v1beta1"
@@ -47,23 +44,6 @@ func TestHelloWorldLocal(t *testing.T) {
 }
 
 func TestHelloWorldGCS(t *testing.T) {
-
-	// Open pipeline.json file and copy it to GCS
-	body, _ := os.Open("pipeline.json")
-
-	bucketName := os.Getenv("TF_VAR_gcp_bucket")
-	objectName := os.Getenv("TF_VAR_object_name")
-
-	storage_ctx := context.Background()
-	storage_client, _ := storage.NewClient(storage_ctx)
-	w := storage_client.Bucket(bucketName).Object(objectName).NewWriter(storage_ctx)
-	w.ContentType = "application/json"
-
-	io.Copy(w, body)
-	w.Close()
-
-	// Delete the object from GCS after the test
-	defer storage_client.Bucket(bucketName).Object(objectName).Delete(storage_ctx)
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Directory where main.tf for test is
