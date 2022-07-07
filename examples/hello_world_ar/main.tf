6fbc657434d7e9ef7fc74b59915e38d3605ede23
Jonny Browning <jonathan.browning@datatonic.com>
2022-07-07 12:24:31 +0100

    test: added test for AR pipeline spec

---
+++ examples/hello_world_ar/main.tf
@@ -0,0 +1,16 @@
+module "hello_world_pipeline" {
+  source                 = "../../"
+  project                = var.project
+  vertex_region          = "europe-west2"
+  cloud_scheduler_region = "europe-west2"
+  pipeline_spec_path     = "https://europe-west2-kfp.pkg.dev/${var.project}/${var.ar_repository}/hello-world/latest"
+  parameter_values = {
+    "text" = "Hello, world!"
+  }
+  gcs_output_directory         = "gs://my-bucket/my-output-directory"
+  vertex_service_account_email = "my-vertex-service-account@my-gcp-project-id.iam.gserviceaccount.com"
+  time_zone                    = "UTC"
+  schedule                     = "0 0 * * *"
+  cloud_scheduler_job_name     = "pipeline-from-local-spec"
+
+}
